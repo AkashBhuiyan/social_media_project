da888bec7aa66f7ac6f4879088e72da7304fa235
This_is_Akash <akash.bhuiyan@gmail.com>
2018-09-18 19:52:42 +0600

    account app added.

--- social_media_proj/urls.py
+++ social_media_proj/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'accounts/', include('accounts.urls', namespace='accounts')),
     url(r'accounts/', include('django.contrib.auth.urls')),
+    url(r'^test/$',views.TestPage.as_view(),name='test'),
+    url(r'^thanks/$', views.ThanksPage.as_view(), name='thanks')
 ]
